583bfd1728c1445839f9707e682f1079dac4c78d
shota <si@primary-meta-works.com>
2015-01-03 17:52:31 +0900

    2015年1月3日 17時50分
    -v0.1.2-2 -α
    
    [追加]
    学生確認用のDialogFragmentの枠組みを作成しました.
    
    ESL忘れ申請ボタン・ESL忘れ解除ボタンのリスナー実装を次にしてください.

--- app/src/main/java/jp/pmw/test_en_revolution/CustomDialogFragment.java
+++ app/src/main/java/jp/pmw/test_en_revolution/CustomDialogFragment.java
@@ -13,7 +13,9 @@ import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import jp.pmw.test_en_revolution.confirm_class_plan.AttendanceState;
 import jp.pmw.test_en_revolution.confirm_class_plan.Message;
+import jp.pmw.test_en_revolution.confirm_class_plan.Seat;
 import jp.pmw.test_en_revolution.confirm_class_plan.Student;
 import jp.pmw.test_en_revolution.confirm_class_plan.TotalAttendance;
 
@@ -60,19 +62,26 @@ public class CustomDialogFragment extends DialogFragment{
         TextView fullNameTextview = (TextView)dialog.findViewById(R.id.title_full_name_textView);
         fullNameTextview.setText(this.attendanceStudent.getFullName());
 
-        //着座していセット
+        //出欠カラーを取得
+        int mainFrameResorceColor = getFrameColorResorce();
 
+        //フレーム全体の色を選択
+        setFrameBackgoundColor(dialog,mainFrameResorceColor);
+        //着座していセット
+        setThisClassTimeSittingPosition(dialog);
         //総出席状況セット
         setTotalAttendanceSituation(dialog);
         //メッセージセット
         setMessage(dialog);
         //ESL忘れセット＆出席確認時刻セット
         setForgotESL(dialog);
-       //出席確認時間セット
-        setConfirmTodayAttendanceTime(dialog);
+        //出席確認時間セット
+        //setConfirmTodayAttendanceTime(dialog);
 
         // 閉じるボタン ボタンのリスナ
-        dialog.findViewById(R.id.positive_button).setOnClickListener(new OnClickListener() {
+        Button closeBtn = (Button)dialog.findViewById(R.id.positive_button);
+        closeBtn.setBackgroundColor(mainFrameResorceColor);
+        closeBtn.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
                 dismiss();
@@ -80,7 +89,72 @@ public class CustomDialogFragment extends DialogFragment{
         });
         return dialog;
     }
+    /**
+     * Created by scr on 2015/1/3.
+     * getAttendanceStateメソッド
+     * 学生の今回の授業の出席状態を返します.
+     * @return AttendanceState タップされた今回の授業出席状態
+     */
+    private AttendanceState getAttendanceState(){
+        return this.attendanceStudent.getThisClassTime().getThisClassAttendanceState();
+    }
+    /**
+     * Created by scr on 2015/1/3.
+     * getFrameColorResorceメソッド
+     * 本日の出席状態カラーを取得します.
+     * @return int 本日の出席カラー
+     */
+    private int getFrameColorResorce(){
+        int resorce = this.getResources().getColor(R.color.gray);
+        AttendanceState todayAtt = getAttendanceState();
+        if(todayAtt.getTempAttendanceState() == 1){
+            //グレー
+        }else{
+            //緑か赤
+            if(todayAtt.getConfirmTime() != null){
+                //出席者
+                resorce = this.getResources().getColor(R.color.yellowGreen);
+            }else{
+                //欠席者
+                resorce = this.getResources().getColor(R.color.darkRed);
+            }
+        }
+        return resorce;
+    }
+    /**
+     * Created by scr on 2015/1/3.
+     * setThisClassTimeSittingPositionメソッド
+     * 本日の着席位置を表示します..
+     * @param d ダイアログ
+     */
+    private void setThisClassTimeSittingPosition(Dialog d){
+        LinearLayout sittingPosition = getLinearLayout(d,R.id.today_seat_positon_linearLayout);
+        Seat thisClassSitting = this.attendanceStudent.getThisClassTime().getThisClassSittingPosition();
+        if(thisClassSitting == null){
+            //着座指定なし
+            sittingPosition.setVisibility(View.GONE);
+        }else{
+            //着座指定あり
+            sittingPosition.setVisibility(View.VISIBLE);
+            String seatPosition = thisClassSitting.getRow() + this.getString(R.string.seat_number_separetar) + thisClassSitting.getColumn();
+            TextView tv = getTextView(d,R.id.today_seat_positon_textView);
+            tv.setText(seatPosition);
+        }
+    }
 
+    /**
+     * Created by scr on 2015/1/3.
+     * setFrameBackgoundColorメソッド
+     * 本日の出席状態カラーを取得します.
+     * @param d ダイアログ
+     * @param resorce 出席カラーのリソース
+     */
+    private void setFrameBackgoundColor(Dialog d,int resorce){
+        LinearLayout outerFrameLayout = getLinearLayout(d,R.id.linearLayout);
+        LinearLayout titleLayout = getLinearLayout(d,R.id.title_linearLayout);
+        //outerFrameLayout.setBackgroundColor(resorce);
+        titleLayout.setBackgroundColor(resorce);
+    }
 
     private void setTodaySeatPosition(Dialog d){
         /*if(){
@@ -150,9 +224,10 @@ public class CustomDialogFragment extends DialogFragment{
     private void setForgotESL(Dialog d){
         //ESL忘れレイアウト
         LinearLayout layout = getLinearLayout(d,R.id.forgot_esl_linearLayout);
-        if(this.attendanceStudent.getAttendance().getTempAttendanceState() == 0
-                &&this.attendanceStudent.getAttendance().getRequestForgotESLTime() == null
-                &&this.attendanceStudent.getAttendance().getConfirmTime() != null){
+        AttendanceState attState = this.attendanceStudent.getThisClassTime().getThisClassAttendanceState();
+        if(attState.getTempAttendanceState() == 0
+                &&attState.getRequestForgotESLTime() == null
+                &&attState.getConfirmTime() != null){
             //ACKですでに出席済み.
             layout.setVisibility(View.GONE);
         }else{
@@ -160,7 +235,8 @@ public class CustomDialogFragment extends DialogFragment{
             layout.setVisibility(View.VISIBLE);
             Button releaseBtn = getButton(d,R.id.dialog_custom_forgot_esl_release_button);
             Button requestBtn = getButton(d,R.id.dialog_custom_forgot_esl_button);
-            if(this.attendanceStudent.getAttendance().getRequestForgotESLTime() != null){
+            
+            if(attState.getRequestForgotESLTime() != null){
                 //ESL忘れ申請をすでに行っているので
                 //解除ボタンを出す.
                 releaseBtn.setVisibility(View.VISIBLE);
@@ -181,20 +257,19 @@ public class CustomDialogFragment extends DialogFragment{
     private void setConfirmTodayAttendanceTime(Dialog d){
         //出席時刻確認レイアウト
         LinearLayout confirmLayout = this.getLinearLayout(d,R.id.attendee_confirm_time_linearLayout);
-        if(this.attendanceStudent.getAttendance().getTempAttendanceState() == 0
-                &&this.attendanceStudent.getAttendance().getRequestForgotESLTime()==null
-                &&this.attendanceStudent.getAttendance().getConfirmTime() != null){
+        AttendanceState thisClassAtt = getAttendanceState();
+        if(thisClassAtt.getTempAttendanceState() == 0
+                &&thisClassAtt.getRequestForgotESLTime()==null
+                &&thisClassAtt.getConfirmTime() != null){
             //出席時刻確認レイアウト表示
             TextView confirmTV = this.getTextView(d,R.id.attendee_confirm_time_textViewt);
-            confirmTV.setText(this.attendanceStudent.getAttendance().getConfirmTime());
+            confirmTV.setText(thisClassAtt.getConfirmTime());
             confirmLayout.setVisibility(View.VISIBLE);
         }else{
             //出席時刻確認レイアウト非表示
             confirmLayout.setVisibility(View.GONE);
         }
     }
-
-
 
     private LinearLayout getLinearLayout(Dialog d,int resorce){
         return (LinearLayout)d.findViewById(resorce);
@@ -205,8 +280,4 @@ public class CustomDialogFragment extends DialogFragment{
     private Button getButton(Dialog d,int resorce){
         return (Button)d.findViewById(resorce);
     }
-
-
-
-
 }
