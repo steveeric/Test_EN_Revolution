4d95f3ab07fa9ab03b266a1a9503994892f87edc
Shota Ito <si@pmw-nagoya.co.jp>
2016-12-17 12:29:41 +0900

    ■version 0.4.10 ACKインジケーターの表示調整終了

--- app/src/main/java/jp/pmw/test_en_revolution/attendee/IndicatorEnum.java
+++ app/src/main/java/jp/pmw/test_en_revolution/attendee/IndicatorEnum.java
@@ -3,7 +3,6 @@ package jp.pmw.test_en_revolution.attendee;
 import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.support.v4.content.ContextCompat;
-
 import jp.pmw.test_en_revolution.R;
 import static jp.pmw.test_en_revolution.attendee.Indicator.ACK;
 import static jp.pmw.test_en_revolution.attendee.Indicator.DEF;
@@ -13,14 +12,15 @@ import static jp.pmw.test_en_revolution.attendee.Indicator.TMF;
 
 /**
  * Created by si on 2016/12/17.
+ * ACKインジケーター
  */
 
 public enum IndicatorEnum {
     DEFAULT_SIGNAL(DEF, R.drawable.signal_gray),
     NACK_SIGNAL(NAC, R.drawable.signal_red),
     ACK_SIGNAL(ACK, R.drawable.signal_green),
-    TARGET_MANUAL_FOLLOW_SIGNAL(TMF, R.drawable.signal_tie_green),
-    NOT_TARGET_MANUAL_FOLLOW_SIGNAL(NMF, R.drawable.signal_tie_green);
+    TARGET_MANUAL_FOLLOW_SIGNAL(TMF, R.drawable.signal_target_manual_follow),
+    NOT_TARGET_MANUAL_FOLLOW_SIGNAL(NMF, R.drawable.signal_not_target_manual_follow);
 
     private final String signal;
     private final int signalResorce;
